2a33703f0a74066aa6d031148169513c5480b554
15978104402 <guhui4402@foxmail.com>
2023-04-05 21:36:07 +0800

    直接插入排序

---
+++ InsertSort/Test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void InsertSort(int* a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int end = i;
+        int tmp = a[end + 1];
+        while (end >= 0)
+        {
+            if (tmp < a[end])
+            {
+                a[end + 1] = a[end];
+                end--;
+            }
+            else
+                break;
+        }
+        a[end + 1] = tmp;
+    }
+}
+
+int main()
+{
+    int arr[10] = { 9, 4, 21, 31, 6, 89, 2, 1, 9, 24 };
+    InsertSort(arr, sizeof(arr) / sizeof(int));
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    putchar('\n');
+    return 0;
+}
