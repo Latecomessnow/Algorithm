99672cf5d415b2194299f504335299b8c692ea6d
15978104402 <guhui4402@foxmail.com>
2023-04-17 17:25:12 +0800

    冒泡排序

--- BubbleSort/Test.c
+++ BubbleSort/Test.c
@@ -7,51 +7,36 @@ void Swap(int *a, int *b)
     *b = tmp;
 }
 
-void AdjustDown(int *a, int n, int parent)
+void BubbleSort(int *a, int n)
 {
-    int child = parent * 2 + 1;
-    while (child < n)
+    // bool flag = true;
+    int flag = 1;
+    for (int i = 0; i < n; i++)
     {
-        if (child + 1 < n && a[child + 1] > a[child])
-            child++;
-        if (a[child] > a[parent])
+        for (int j = 0; j < n - 1 - i; j++)
         {
-            Swap(&a[child], &a[parent]);
-            parent = child;
-            child = parent * 2 + 1;
+            if (a[j] > a[j + 1])
+            {
+                Swap(&a[j], &a[j + 1]);
+                flag = 0;
+            }
         }
-        else
-            break;
-    }
-}
-
-void HeapSort(int *a, int n)
-{
-    // 堆排序前需要先建堆，从倒数第一个非叶子节点开始建堆
-    for (int i = (n - 1 - 1) / 2; i >= 0; i--)
-    {
-        AdjustDown(a, n, i);
-    }
-    int end = n - 1;
-    while (end)
-    {
-        // 每次都把最大的堆定数据换到最后，然后从堆顶开始向下调整
-        Swap(&a[0], &a[end]);
-        AdjustDown(a, end, 0);
-        end--;
+        if (flag == 1)
+            return;
     }
 }
 
 int main()
 {
-    int arr[10] = {2, 10, 42, 3, 56, 1, 8, 90, 45, 2};
+    // int arr[10] = {2, 10, 42, 3, 56, 1, 8, 90, 45, 2};
+    int arr[10] = {1, 2, 3, 4, 10, 6, 7, 8, 9, 10};
     for (int i = 0; i < 10; i++)
     {
         printf("%d ", arr[i]);
     }
     putchar('\n');
     putchar('\n');
-    HeapSort(arr, sizeof(arr) / sizeof(int));
+    BubbleSort(arr, sizeof(arr) / sizeof(int));
     for (int i = 0; i < 10; i++)
     {
         printf("%d ", arr[i]);
